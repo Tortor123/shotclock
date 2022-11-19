ac519bd5bfd559e9625a32862eecd9f2baeef74f
Tortor123 <100969221+Tortor123@users.noreply.github.com>
2022-11-19 16:23:02 -0500

    lets see
    This is an interactive score board.

---
+++ co5e34315b45fb22108f96106/index.js
@@ -0,0 +1,32 @@
+let scoreEl = document.getElementById("score-btn")
+let scoreEl1 = document.getElementById("score-btn1")
+let result = 0
+function plusOne(){
+    result += 1
+    scoreEl.textContent =  result
+}
+
+function plusTwo(){
+    result += 2
+    scoreEl.textContent =  result
+}
+
+function plusThree(){
+     result += 3
+    scoreEl.textContent =  result
+}
+
+function plusOne1(){
+    result += 1
+    scoreEl1.textContent =  result
+}
+
+function plusTwo1(){
+    result += 2
+    scoreEl1.textContent =  result
+}
+
+function plusThree1(){
+    result += 
+    scoreEl1.textContent =  result
+}
